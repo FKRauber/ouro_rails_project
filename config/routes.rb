69f36785b83feded750ae378dce1c11ab80b2afb
FKRauber <Fabienne.Rauber@gmail.com>
2018-11-28 14:29:19 -0500

    basic config/routes ; app/views folder structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "static_pages#home"
+
+  resources :users
+  resources :treasures
+
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  post "/theories/new", to: "theories#new"
 end
