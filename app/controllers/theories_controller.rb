5d702c02ad0080f37d6af2728f11056166df507f
FKRauber <Fabienne.Rauber@gmail.com>
2018-11-29 22:03:35 -0500

    finished basic views ; filled basic threasure and theory controllers

--- app/controllers/theories_controller.rb
+++ app/controllers/theories_controller.rb
@@ -1,23 +1,39 @@
 class TheoriesController < ApplicationController
+  before_action :theory, except: [:new, :create, :index]
+  
   def index
+    @theories = Theory.all
   end
 
   def show
+    @theory = theory
   end
 
   def new
+    @theory = Theory.new
   end
-
   def create
+    @theory = Theory.create(theory_params)
+    if @theory.save
+      redirect_to @theory, notice: "Theory was successfully saved"
+    else
+      render :new
+    end
   end
 
   def edit
   end
-
   def update
+    if @theory.update(theory_params)
+      redirect_to @theory, notice: "Theory was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @theory.destroy
+    redirect_to theories_url, notice: "Theory was successfully destroyed"
   end
 
   private
--- app/controllers/treasures_controller.rb
+++ app/controllers/treasures_controller.rb
@@ -1,22 +1,48 @@
 class TreasuresController < ApplicationController
+  before_action :treasure, except: [:new, :create, :index]
+  
   def index
+    @treasures = Treasure.all
   end
 
   def show
+    @treasure = treasure
   end
 
   def new
+    @treasure = Treasure.new
   end
-
   def create
+    @treasure = Treasure.create(treasure_params)
+    if @treasure.save
+      redirect_to @treasure, notice: "Treasure was successfully saved"
+    else
+      render :new
+    end
   end
 
   def edit
   end
-
   def update
+    if @treasure.update(treasure_params)
+      redirect_to @treasure, notice: "Treasure was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @treasure.destroy
+    redirect_to treasures_url, notice: "Treasure was successfully destroyed"
+  end
+
+  private
+
+  def treasure
+    @treasure = Treasure.find(params[:id])
+  end
+
+  def treasure_params
+    params.require(:treasure).permit(:name, :description)
   end
 end
