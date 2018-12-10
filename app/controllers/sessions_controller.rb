fbefc865d4af5423de224f2693231a8553e22bea
FKRauber <Fabienne.Rauber@gmail.com>
2018-12-10 15:10:13 -0500

    updated links and added css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   def create
   	return redirect_to(controller: 'sessions', action: 'new') if !params[:username] || params[:username].empty?
   	session[:username] = params[:username]
-  	redirect_to controller: 'application', action: 'hello'
+  	redirect_to controller: 'static_pages', action: 'home'
   end
 
   def destroy
-  	session.delete :name
-  	redirect_to controller: 'application', action: 'hello'
+  	session.delete :username
+  	redirect_to controller: 'static_pages', action: 'home'
   end
 end
