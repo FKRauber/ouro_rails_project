2a5f7fd11a80e22e5538cce30f6408f3d02be2db
FKRauber <fabienne.rauber@gmail.com>
2019-01-15 12:04:38 -0500

    updated omniauth params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_190250) do
+ActiveRecord::Schema.define(version: 2019_01_15_170144) do
 
   create_table "theories", force: :cascade do |t|
     t.string "name"
@@ -40,8 +40,11 @@ ActiveRecord::Schema.define(version: 2019_01_09_190250) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "uid"
+    t.string "uid"
     t.string "email"
+    t.string "provider"
+    t.string "name"
+    t.text "image"
   end
 
 end
